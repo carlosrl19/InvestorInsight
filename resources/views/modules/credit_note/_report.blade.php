163ca5c1d4cce9e13908036669dbcc43a4bba3fb
Carlos Rodriguez <85375012+carlosrl19@users.noreply.github.com>
2024-05-15 15:13:51 -0600

    Se hicieron cambios en el reporte de notas crédito

--- resources/views/modules/credit_note/_report.blade.php
+++ resources/views/modules/credit_note/_report.blade.php
@@ -1,32 +1,40 @@
 <div class="main">
+	<!-- Credit's note title -->
 	<div class="title-note mt-center">
 		NOTA CREDITO POR LPS {{ number_format($creditNote->creditNote_amount,2 ) }}
 	</div>
 
-	<div class="mt-4">
+	<!-- Credit's note body -->
+	<div class="body-note mt-4 mb-4">
 		Por la presente, se emite la siguiente nota de crédito a favor del inversionista <strong>{{ $creditNote->investor->investor_name }}</strong>, identificado con su número de identidad 
 		<strong>{{ $creditNote->investor->investor_dni }}</strong>, número de teléfono <strong>{{ $creditNote->investor->investor_phone }}</strong>, una nota de crédito con un valor total de Lps. <strong>{{ number_format($creditNote->creditNote_amount,2) }}</strong>.
 	</div>
 
+	<!-- Credit's note reason title -->
 	<div class="reason-note-title mt-4 mb-2">
 		Motivo de emisión
 	</div>
 
+	<!-- Credit's note readon body -->
 	<div class="reason-note-body">
 		{{ $creditNote->creditNote_description }}
 	</div>
 
+	<!-- Credit's note issue date -->
 	<div class="mt-6">
-		Emitido por Inversiones ROBENIOR en San Pedro Sula, Cortés, Honduras, a los {{ $dia }} días del mes de {{ $mes }} del año {{ $anio }}.
+		Documento emitido por Inversiones ROBENIOR en San Pedro Sula, Cortés, Honduras, a los {{ $dia }} días del mes de {{ $mes }} del año {{ $anio }}.
 	</div>
 
-	<div class="mt-6 ml-center">
+	<!-- Credit's note Robenior signature -->
+	<div class="mt-4 ml-center">
 		&nbsp;&nbsp;<img src="static/Firma-ejemplo.png" alt="Logo" height="80px" style="position: absolute; margin-top: -25px"><br>
+		&nbsp;&nbsp;<img src="static/sello-ejemplo.png" alt="Logo" height="80px" style="position: absolute; margin-top: -35px; margin-left: 30px"><br>
 		_________________<br>
 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dariel Moncada<br>
-		&nbsp;<strong>Gerente ROBENIOR</strong>
+		<strong style="margin-left: -12px">Gerente administrativo</strong>
 	</div>
 
+	<!-- Legal footer -->
 	<div class="footer mt-6">
 		Este documento contiene la firma y sello oficial de la empresa <i>ROBENIOR</i>. Cualquier uso no autorizado, reproducción, alteración o falsificación de esta firma o sello constituirá una violación de los derechos de propiedad intelectual de la empresa y será sujeto a acciones legales.
 		Se advierte a toda persona que tenga acceso a este documento que el uso indebido de la firma o sello de la empresa será considerado como un acto ilícito y se procederá legalmente en consecuencia.
@@ -38,6 +46,10 @@
 	* {
 		font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
 		text-align: justify;
+	}
+
+	.body-note{
+		font-size: 16px;
 	}
 
 	.mt-2 {
