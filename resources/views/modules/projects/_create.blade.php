963a62fcd2dedeb88ba0b1a5f370032f85b75012
Carlos Rodriguez <85375012+carlosrl19@users.noreply.github.com>
2024-05-18 08:50:36 -0600

    Agregado if en creación de projects para mostrar solo inversioinstas disponibles

--- resources/views/modules/projects/_create.blade.php
+++ resources/views/modules/projects/_create.blade.php
@@ -98,8 +98,10 @@
                                         <div class="card-header">Listado de inversionistas disponibles para proyectos <button type="button" class="btn btn-primary" style="margin: auto;" id="add_investor_btn"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-user-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M16 19h6" /><path d="M19 16v6" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /></svg></button></div>
                                         <select class="form-select js-example-basic-multiple" id="investor_select" style="font-size: clamp(0.6rem, 3vh, 0.7rem); width: 100%">
                                             <option></option>
-                                            @foreach ($investors as $investor)                                    
-                                                <option value="{{ $investor->id }}">{{ $investor->investor_name }}</option>
+                                            @foreach ($investors as $investor)
+                                                @if($investor->investor_status == 1)
+                                                    <option value="{{ $investor->id }}">{{ $investor->investor_name }}</option>
+                                                @endif
                                             @endforeach
                                         </select>
                                     </div>
