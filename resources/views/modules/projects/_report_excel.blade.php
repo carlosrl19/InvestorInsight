7e447832af17749494de3fdea53f5006c117aad6
Carlos Rodriguez <85375012+carlosrl19@users.noreply.github.com>
2024-06-12 10:47:18 -0600

    Se hicieron cambios en el formato del excel debido a que cuando eran 2 comisionistas la columna G no se mostraba, apenas tenía un width como de 5px

--- resources/views/modules/projects/_report_excel.blade.php
+++ resources/views/modules/projects/_report_excel.blade.php
@@ -9,7 +9,9 @@
         <tr>
             <td></td>
             @foreach($project->investors as $investor)
-                <td style="font-size: 14px; width: 100px; font-weight: bold; background-color: #fff; text-align: left; text-decoration: underline;">PROYECTO {{ $investor->investor_name }} </td>
+                <td style="font-size: 14px; width: 100px; font-weight: bold; background-color: #fff; text-align: left; text-decoration: underline;">
+                    PROYECTO {{ $investor->investor_name }}
+                </td>
             @endforeach
             <td style="background-color: #fff; width: auto;"></td>
             <td style="background-color: #fff; width: 100px;"></td>
@@ -27,10 +29,14 @@
             <td style="background-color: #fff; width: 160px;"></td>
             <td style="background-color: #fff; width: 160px;"></td>
             @if(isset($project->commissioners[1]))
-                <td style="background-color: #fff;"></td>
-                <td style="background-color: #fff; text-align: center; font-weight: bold;">#CP-{{ $project->project_code }}</td>
+                <td style="background-color: #fff; width: 160px;"></td>
+                <td style="background-color: #fff; text-align: center; font-weight: bold;">
+                    #CP-{{ $project->project_code }}
+                </td>
             @else
-                <td style="background-color: #fff; text-align: center; font-weight: bold;">#CP-{{ $project->project_code }}</td>
+                <td style="background-color: #fff; text-align: center; font-weight: bold;">
+                    #CP-{{ $project->project_code }}
+                </td>
                 <td></td>
             @endif
         </tr>
@@ -43,9 +49,9 @@
             <td style="background-color: #fff"></td>
             <td style="background-color: #fff"></td>
             <td style="background-color: #fff;"></td>
-            <td style="background-color: #fff;"></td>
+            <td style="background-color: #fff; width: 120px;"></td> <!-- Asegurar el ancho -->
             @if(isset($project->commissioners[1]))
-                <td style="background-color: #fff;"></td>
+                <td style="background-color: #fff; width: 120px;"></td> <!-- Asegurar el ancho -->
                 <td style="background-color: #fff;"></td>
             @else
                 <td style="background-color: #fff"></td>
@@ -61,9 +67,9 @@
             <td style="background-color: #fff"></td>
             <td style="background-color: #fff"></td>
             <td style="background-color: #fff;"></td>
-            <td style="background-color: #fff;"></td>
+            <td style="background-color: #fff; width: 120px;"></td> <!-- Asegurar el ancho -->
             @if(isset($project->commissioners[1]))
-                <td style="background-color: #fff;"></td>
+                <td style="background-color: #fff; width: 120px;"></td> <!-- Asegurar el ancho -->
                 <td style="background-color: #fff;"></td>
             @else
                 <td style="background-color: #fff"></td>
@@ -76,20 +82,20 @@
             <td></td>
             <td style="background-color: #A9D08E; width: 150px"></td>
             <td></td>
-            <td style="background-color: #fff; font-size: 11px; font-weight: bold;">CAPITAL</td>
+            <td style="background-color: #fff; font-size: 11px; font-weight: bold; text-align: center; ">CAPITAL</td>
             <td style="background-color: #fff; font-size: 11px; font-weight: bold; width: 120px; text-align: center;">GANANCIA TOTAL</td>
             <td style="background-color: #fff; font-size: 11px; font-weight: bold; width: 150px; text-align: center;">
                 {{ implode(' ', array_slice(explode(' ', $investor->investor_name), 0, 1)) }} 50%
             </td>
             @if(isset($project->commissioners[1]))
-                <td style="background-color: #fff; font-size: 11px; font-weight: bold; text-align: center;">
+                <td style="background-color: #fff; font-size: 11px; font-weight: bold; text-align: center; width: 120px;">
                     {{ implode(' ', array_slice(explode(' ', $commissioners->get(1)->commissioner_name ?? '-'), 0, 1)) }} 10%
                 </td>
-                <td style="background-color: #fff; font-size: 11px; font-weight: bold; text-align: center;">
+                <td style="background-color: #fff; font-size: 11px; font-weight: bold; text-align: center; width: 120px;">
                     {{ implode(' ', array_slice(explode(' ', $commissioners[0]->commissioner_name), 0, 1)) }} 40%
                 </td>
             @else
-                <td style="background-color: #fff; font-size: 11px; font-weight: bold; text-align: center;">
+                <td style="background-color: #fff; font-size: 11px; font-weight: bold; text-align: center; width: 120px;">
                     {{ implode(' ', array_slice(explode(' ', $commissioners[0]->commissioner_name), 0, 1)) }} 50%
                 </td>
             @endif
@@ -105,11 +111,11 @@
             <td style="text-align: center; font-weight: bold; border-bottom: 1px solid #000;">L. {{ number_format($project->investors->sum('pivot.investor_profit'), 2) }}</td>
             <td style="text-align: center; font-weight: bold; border-bottom: 1px solid #000; width: 100px; text-decoration: underline; font-weight: bold;">L. {{ number_format($project->investors->sum('pivot.investor_final_profit'), 2) }}</td>
             @if(isset($project->commissioners[1]))
-                <td style="text-align: center; border-bottom: 1px solid #000; width: auto; font-weight: bold;">
+                <td style="text-align: center; border-bottom: 1px solid #000; width: 120px; font-weight: bold;">
                     L. {{ number_format($project->commissioners[1]->pivot->commissioner_commission, 2) }}
                 </td>
             @endif
-            <td style="text-align: center; font-weight: bold; border-bottom: 1px solid #000; width: 100px">L. {{ number_format($project->commissioners[0]->pivot->commissioner_commission, 2) }}</td>
+            <td style="text-align: center; font-weight: bold; border-bottom: 1px solid #000; width: 120px">L. {{ number_format($project->commissioners[0]->pivot->commissioner_commission, 2) }}</td>
             <td style="color: #1F4E82; text-decoration: underline; text-align: left; border-bottom: 1px solid #000;">{{ $project->project_comment }}</td>
         </tr>
     </tbody>
